5f713b0e7bb2c4490c38b3675633dbce43dca3da
AntonySorrento <78973987+AntonySorrento@users.noreply.github.com>
2021-04-24 16:37:51 +0200

    commit

---
+++ printint_M1_ok.c
@@ -0,0 +1,31 @@
+# include <stdio.h>
+# include <stdarg.h>
+
+va_list onebyone(va_list args, int i)
+{
+	int value;
+	
+	value = va_arg(args, int);
+	printf ("%d : %d\n", i, value);
+	return(args);
+}
+
+void print_ints(int num, ...)
+{
+	va_list args;
+	int i;
+
+	va_start(args, num);
+
+	for (i = 0; i < num; i++)
+	{
+		args = onebyone(args, i);
+	}
+	va_end(args);
+}
+int main()
+{
+	print_ints(3, 24, 26, 312);
+	print_ints(2, 256, 512);
+	print_ints(7, 1, 2, 3, 4, 5, 6, 7);
+}
